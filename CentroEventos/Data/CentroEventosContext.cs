fb38cbba3d9e0c2527289919fa83f5743337da94
Catalinda1 <142274765+Catalinda1@users.noreply.github.com>
2025-06-06 18:19:57 -0300

    pruebas

---
+++ CentroEventos/Data/CentroEventosContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using CentroEventos.Aplicacion.entidades; 
+
+namespace CentroEventos.Data
+{
+    public class CentroEventosContext : DbContext
+    {
+        public CentroEventosContext(DbContextOptions<CentroEventosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<EventoDeportivo> EventosDeportivos { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source= CentroEventos.sqlite");
+        }
+    }
+}
